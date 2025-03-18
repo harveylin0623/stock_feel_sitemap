34fcbb3ff656d99cf8f1b9a495507ffe4ac963ee
harvey_lin <harvey_lin@linkwish.com>
2025-03-18 11:52:31 +0800

    將api調整成1000筆做測試

--- server/api/sitemap.js
+++ server/api/sitemap.js
@@ -4,7 +4,7 @@ import { defineSitemapEventHandler } from '#imports'
 export default defineSitemapEventHandler(async () => {
   const ids = []
   const response = await axios({
-    url: 'https://randomuser.me/api/?results=10',
+    url: 'https://randomuser.me/api/?results=1000',
     method: 'get'
   })
 
